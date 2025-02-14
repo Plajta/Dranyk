11f3cda36e6ae00e66536e5afe69964e81d50a79
HelloWorld7894 <danpojhan@protonmail.com>
2025-02-15 00:09:12 +0100

    small bols

--- src/map/main.py
+++ src/map/main.py
@@ -17,9 +17,8 @@ if __name__ == "__main__":
     data = gjson_reader.read_data()
 
     rivers = extract_coordinates_as_lines(data["WGS_vodni_tok"])
-    """
-    process_rails(rivers["features"],gjson_writer)
-    """
+
+    #process_rails(rivers["features"], gjson_writer)
     process_rails(data["WGS_koleje2"]["features"],
                   data["WGS_budova"]["features"],
                   gjson_writer)
