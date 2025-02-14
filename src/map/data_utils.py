b110dd6335584c3321d46ab51dfa8cc9b35c9e7c
HelloWorld7894 <danpojhan@protonmail.com>
2025-02-14 17:04:35 +0100

    added json loader

--- src/map/data_utils.py
+++ src/map/data_utils.py
@@ -1,6 +1,28 @@
 import geojson
 
+import os
 
-def read_geojson(path):
-    with open('data.geojson', 'r') as file:
-        return geojson.load(file)
+
+class GeoJSONreader:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.data = {}
+
+    def __read_geojson__(self, path):
+        with open(path, 'r') as file:
+            return geojson.load(file)
+
+    def read_data(self):
+        for file in os.listdir(self.data_path):
+            if file == ".gitkeep": # ignore gitkeep
+                continue
+
+            file_path_abs = os.path.join(self.data_path, file)
+
+            file_name = file.replace(".geojson", "")
+            print(f"Processed {file_name}")
+            file_content = self.__read_geojson__(file_path_abs)
+
+            self.data[file_name] = file_content
+
+        return self.data
--- src/map/main.py
+++ src/map/main.py
@@ -1,4 +1,14 @@
-import data_utils
+# imports
+from os.path import join
+from pathlib import Path
+
+# relative imports
+from data_utils import GeoJSONreader
+
+ABS_PATH = str(Path(__file__).parents[2])
+DATA_PATH = join(ABS_PATH, "data")
 
 if __name__ == "__main__":
-    print("Bols")
+    gjson_reader = GeoJSONreader(DATA_PATH)
+    data = gjson_reader.read_data()
+    # Like bols
