5239dbc449686c8f12626b315d03d75021cc25a4
Sles <slesusagi@mail.com>
2025-02-14 21:24:41 +0000

    diferent json

--- src/map/geojson_api.py
+++ src/map/geojson_api.py
@@ -10,30 +10,48 @@ sample_geojson = {
     "features": [
         {
             "type": "Feature",
-            "properties": {"name": "Sample Location"},
-            "geometry": {
-                "type": "Point",
-                "coordinates": [10.0, 50.0]  # [longitude, latitude]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {"name": "Sample Polygon"},
+            "properties": {"name": "River Section 1"},
             "geometry": {
                 "type": "Polygon",
                 "coordinates": [
                     [
                         [10.0, 50.0],
-                        [10.5, 50.0],
+                        [10.4, 50.1],
+                        [10.3, 50.3],
                         [10.5, 50.5],
-                        [10.0, 50.5],
+                        [10.7, 50.7],
+                        [10.2, 50.8],
+                        [10.1, 50.6],
+                        [10.1, 50.6],
+                        [9.9, 50.2],
                         [10.0, 50.0]
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "River Section 4 (Connected to Section 3)"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [11.0, 50.4], 
+                        [10.7, 50.5],
+                        [10.5, 50.5],
+                        [10.7, 50.7],
+                        [11.2, 50.5], 
+                        [11.3, 50.2],
+                        [11.1, 50]
+                        
                     ]
                 ]
             }
         }
     ]
 }
+
+
 
 
 
